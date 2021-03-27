86f53319193b522632c4da6a0024321970fb56b8
kwonhygge <cleancode.kwon@gmail.com>
2021-03-28 08:42:17 +0900

    Feat: 테마 설정
    - font 사이즈 별로 설정
    - 자주 사용하는 color 설정

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,13 +1,32 @@
 import { DefaultTheme } from 'styled-components';
 
+const calcRem = (size: number) => `${size / 16}rem`;
+
+const fontSizes = {
+  //  기본 타이틀 사이즈
+  titleSize: calcRem(24),
+
+  // 크기별 분류
+  xSmall: calcRem(13),
+  small: calcRem(14),
+  base: calcRem(16),
+  lg: calcRem(18),
+  xl: calcRem(32),
+};
+
+const colors = {
+  primaryBlue: '#427D96',
+  lightBlue: '#E4EDF2',
+  orange: '#FF844B',
+  gray: '#C4C4C4',
+  lightGray: '#F5F5F5',
+};
+
+//TODO margin & padding도 설정
+
 export const theme: DefaultTheme = {
-  // colors: {
-  //     black: '#1e1f1d',
-  //     yellow: '#edb83c',
-  //     orange: '#eb7952',
-  //     gray: '#6e6e6e',
-  //     gray_background: '#f5f5f5',
-  // },
+  fontSizes,
+  colors,
 };
 
 const customMediaQuery = (maxWidth: number): string =>
