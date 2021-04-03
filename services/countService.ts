050040e1692ebb4f6ceb921ed7171cc280f8ea81
codeminnam <code.minnam@gmail.com>
2021-04-04 02:05:02 +0900

    fix: 리턴 타입 변경

--- services/countService.ts
+++ services/countService.ts
@@ -3,8 +3,8 @@ import axios from 'axios';
 const BACK_API_URL = 'https://day-with-taste.herokuapp.com';
 
 export default class CountService {
-  public static async getCounts(): Promise<number> {
-    const response = await axios.get<number>(`${BACK_API_URL}/visit`);
+  public static async getCounts(): Promise<Object> {
+    const response = await axios.get<Object>(`${BACK_API_URL}/visit`);
     return response.data;
   }
 }
