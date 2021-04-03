f68310e5321effbb65d4cc96fb0b5034bfc55100
codeminnam <code.minnam@gmail.com>
2021-04-04 01:54:53 +0900

    fix: count 리턴 타입 변경

--- services/songService.ts
+++ services/songService.ts
@@ -5,18 +5,21 @@ const BACK_API_URL = 'https://day-with-taste.herokuapp.com';
 
 export default class SongService {
   public static async getSongs(keyword: string): Promise<Song[]> {
-    const response = await axios.post<Song[]>(`${BACK_API_URL}/youtube`, {
+    const response = await axios.get<Song[]>(`${BACK_API_URL}/youtube`, {
       data: keyword,
     });
-
+    console.log(keyword);
+    console.log(response.data);
     return response.data;
   }
 
-  public static async getResult(music: string, result: string): Promise<Song> {
+  public static async postResult(music: string, result: string): Promise<Song> {
+    console.log(music);
     const response = await axios.post<Song>(`${BACK_API_URL}/result`, {
-      data: { music, result },
+      data: { music: music, result: result },
     });
 
+    console.log(response.data);
     return response.data;
   }
 }
