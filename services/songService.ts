ad88fa03e26e8759bc640c8e54611f43b8389f36
codeminnam <code.minnam@gmail.com>
2021-04-06 20:44:28 +0900

    fix: SongService 구문 수정

--- services/songService.ts
+++ services/songService.ts
@@ -6,20 +6,18 @@ const BACK_API_URL = 'https://day-with-taste.herokuapp.com';
 export default class SongService {
   public static async getSongs(keyword: string): Promise<Song[]> {
     const response = await axios.get<Song[]>(`${BACK_API_URL}/youtube`, {
-      data: keyword,
+      params: { keyword },
     });
-    console.log(keyword);
-    console.log(response.data);
+
     return response.data;
   }
 
   public static async postResult(music: string, result: string): Promise<Song> {
-    console.log(music);
     const response = await axios.post<Song>(`${BACK_API_URL}/result`, {
-      data: { music: music, result: result },
+      music,
+      result,
     });
 
-    console.log(response.data);
     return response.data;
   }
 }
