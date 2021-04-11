a7b06816adfb71154b2c021d67de64e1fb94d5ca
codeminnam <code.minnam@gmail.com>
2021-04-11 17:39:14 +0900

    Feat: 결과 배열 기능 작성

--- components/layout/Questionnaire.tsx
+++ components/layout/Questionnaire.tsx
@@ -1,11 +1,13 @@
 import Router from 'next/router';
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
 import { Question } from '../../interfaces';
 import Button from '../common/Button';
 import Circle from '../common/Circle';
 import Label from '../common/Label';
 import TitleText from '../common/TitleText';
+import { setResult as setResultAction } from '../../reducers/songReducer';
 
 type Props = {
   item: Question;
@@ -41,36 +43,45 @@ const AnswerButton = styled(Button)`
   }
 `;
 
-const Questionnaire = ({ item }: Props) => (
-  <QuestionnaireContainer>
-    <UpperContainer>
-      <HeadCircle size="small" />
-      <ProgressLabel index={item.id} />
-      <QuestionText>{item.question}</QuestionText>
-    </UpperContainer>
-    <ButtonContainer>
-      <AnswerButton
-        label={item.answerA}
-        onClick={() => {
-          if (item.id === 11) {
-            Router.push('/loading/end');
-          } else {
-            Router.push(`/questionnaires/${item.id + 1}`);
-          }
-        }}
-      />
-      <AnswerButton
-        label={item.answerB}
-        onClick={() => {
-          if (item.id === 11) {
-            Router.push('/loading/end');
-          } else {
-            Router.push(`/questionnaires/${item.id + 1}`);
-          }
-        }}
-      />
-    </ButtonContainer>
-  </QuestionnaireContainer>
-);
+const Questionnaire = ({ item }: Props) => {
+  const [result, setResult] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  const dispatch = useDispatch();
+  let resultArr = [];
+
+  const handleButtonClick = (answer: string) => {
+    resultArr = [
+      ...result.slice(0, item.id - 1),
+      answer === 'A' ? 0 : 1,
+      ...result.slice(item.id, result.length),
+    ];
+    setResult(resultArr);
+    if (item.id === 11) {
+      dispatch(setResultAction(resultArr.join('')));
+      Router.push('/loading/end');
+    } else {
+      Router.push(`/questionnaires/${item.id + 1}`);
+    }
+  };
+
+  return (
+    <QuestionnaireContainer>
+      <UpperContainer>
+        <HeadCircle size="small" />
+        <ProgressLabel index={item.id} />
+        <QuestionText>{item.question}</QuestionText>
+      </UpperContainer>
+      <ButtonContainer>
+        <AnswerButton
+          label={item.answerA}
+          onClick={() => handleButtonClick('A')}
+        />
+        <AnswerButton
+          label={item.answerB}
+          onClick={() => handleButtonClick('B')}
+        />
+      </ButtonContainer>
+    </QuestionnaireContainer>
+  );
+};
 
 export default Questionnaire;
