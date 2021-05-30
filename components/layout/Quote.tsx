81505ce4295d087b8128049ca3906b76f50f81a0
codeminnam <code.minnam@gmail.com>
2021-05-30 19:19:46 +0900

    Fix: 빌드 오류 수정

--- components/layout/Quote.tsx
+++ components/layout/Quote.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import Circle from '../common/Circle';
 import PrimaryText from '../common/PrimaryText';
 import SecondaryText from '../common/SecondaryText';
--- components/layout/StartDay.tsx
+++ components/layout/StartDay.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styled, { css } from 'styled-components';
 import Circle from '../common/Circle';
 import PrimaryText from '../common/PrimaryText';
-import SecondaryText from '../common/SecondaryText';
 
 type QuoteProps = {
   themeColor?: 'light' | 'dark';
@@ -26,24 +25,6 @@ const TitlePrimaryText = styled(PrimaryText)<{ themeColor?: 'light' | 'dark' }>`
   font-weight: normal;
   font-size: 16px;
   line-height: 24px;
-  ${({ themeColor }) => {
-    if (themeColor === 'light')
-      return css`
-        color: #fff;
-      `;
-    if (themeColor === 'dark')
-      return css`
-        color: ${({ theme }) => theme.colors.primaryBlue};
-      `;
-  }}
-`;
-const SmallSecondaryText = styled(SecondaryText)<{
-  themeColor?: 'light' | 'dark';
-}>`
-  font-style: normal;
-  font-weight: normal;
-  font-size: 14px;
-  line-height: 13px;
   ${({ themeColor }) => {
     if (themeColor === 'light')
       return css`
