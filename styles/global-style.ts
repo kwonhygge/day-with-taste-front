f8999e8c98152616bd1c0012ac58143107981409
codeminnam <code.minnam@gmail.com>
2021-06-21 16:00:13 +0900

    fix: fix height issue

--- styles/global-style.ts
+++ styles/global-style.ts
@@ -9,6 +9,8 @@ export const GlobalStyle = createGlobalStyle`
     body{
       font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
       max-width: 100%;
+      min-height:100vh;
+      min-height: -webkit-fill-available;
     }
     a {
       color: inherit;
